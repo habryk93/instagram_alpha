a0f4ff2484ff55440d25806613960653d0de369d
habryk <habryk93@gmail.com>
2017-10-11 23:24:08 +0200

    Fixed url bug

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -3,7 +3,7 @@ from django.conf.urls import url
 from dashboard.views import DashboardView, PictureDetailView, PictureAddView, PictureDeleteView
 
 urlpatterns = [
-    url(r'^picture/add', PictureAddView.as_view(), name='picture_add'),
+    url(r'^picture/add/', PictureAddView.as_view(), name='picture_add'),
     url(r'^picture/delete/(?P<slug>[-\w]+)', PictureDeleteView.as_view(), name='picture_delete'),
     url(r'^picture/(?P<slug>[-\w]+)', PictureDetailView.as_view(), name='picture_info'),
     url(r'^', DashboardView.as_view(), name='dashboard'),
